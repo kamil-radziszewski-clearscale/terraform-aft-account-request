0975f571ecaaf535ec69c1d3f3ed96157d1cbeeb
Kamil Radziszewski <kamil.radziszewski@clearscale.com>
2023-09-23 10:46:01 +0200

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,3 @@
-module "sandbox" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "test1@radziszewski.net"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "AFT"
-    SSOUserEmail              = "test1@radziszewski.net"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
 module "test" {
   source = "./modules/aft-account-request"
 
